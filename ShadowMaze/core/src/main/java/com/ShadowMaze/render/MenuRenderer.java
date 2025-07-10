404d0c4ef4f81decae5418bce13c0e20391740a3
yuohta819 <thunhan798@gmail.com>
2025-07-10 12:44:34 +0700

    f

--- ShadowMaze/core/src/main/java/com/ShadowMaze/render/MenuRenderer.java
+++ ShadowMaze/core/src/main/java/com/ShadowMaze/render/MenuRenderer.java
@@ -17,7 +17,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.kotcrab.vis.ui.widget.VisLabel;
 
 import java.util.ArrayList;
 
